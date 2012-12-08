c866a0147bc6743e096325f8d59eb1508f691357
Emilien Taque <etaque@gmail.com>
2012-12-08 10:42:02 +0100

    Azimuth conversion was wrong

--- src/main/scala/fr/skiffr/oceanrider/Util.scala
+++ src/main/scala/fr/skiffr/oceanrider/Util.scala
@@ -6,6 +6,6 @@ import org.geotools.referencing.GeodeticCalculator
 object Util {
   def knotToMps(knot: Double): Double = knot * 1.852 * 1000 / 3600
 
-  def degreesToAzimuth(d: Double) = if (d > 180) d - 180 else d
-  def azimuthToDegrees(a: Double) = if (a < 0) a + 180 else a
+  def degreesToAzimuth(d: Double) = if (d > 180) d - 360 else d
+  def azimuthToDegrees(a: Double) = if (a < 0) a + 360 else a
 }
