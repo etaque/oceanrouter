47fcf51f8621b6c8fce8b5f67c85cc598118f1c7
Emilien Taque <etaque@gmail.com>
2013-01-06 09:14:32 +0100

    GRIB Download feature... problem: GRIB2 only

--- src/main/scala/oceanrouter/core/WeatherContext.scala
+++ src/main/scala/oceanrouter/core/WeatherContext.scala
@@ -3,6 +3,8 @@ package oceanrouter.core
 import net.sourceforge.jgrib._
 import java.util.Calendar
 import org.joda.time.DateTime
+import io.Source
+import java.io.ByteArrayInputStream
 
 
 case class UVRecords(u: GribRecord, v: GribRecord)
@@ -40,6 +42,14 @@ class WeatherContext(grib: GribFile) {
 }
 
 object WeatherContext {
-  private val gribFile = new GribFile("/Users/emilien/Downloads/zyGrib_mac-6.0.2/grib/20121127_213227_.grb")
-  val current = new WeatherContext(gribFile)
+  //private val gribFile = new GribFile("/Users/emilien/Downloads/zyGrib_mac-6.0.2/grib/20121127_213227_.grb")
+  //val current = new WeatherContext(gribFile)
+  val current = new WeatherContext(loadGrib("2013010506"))
+
+  def loadGrib(date: String) = {
+    val source = Source.fromURL("http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs.pl?file=gfs.t06z.pgrbf00.grib2" +
+      "&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs." +
+      date)
+    new GribFile(new ByteArrayInputStream(source.map(_.toByte).toArray))
+  }
 }
